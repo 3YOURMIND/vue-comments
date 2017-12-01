bffe7aea48d29e30db12d38e8fad0e74bc3ba296
Kevin Peters <kpe@3yourmind.com>
2017-12-01 21:21:55 +0100

    refactor(components): refactor textarea component to fit vue style guide

--- src/components/Textarea.vue
+++ src/components/Textarea.vue
@@ -1,25 +1,57 @@
 <template>
   <textarea
+    ref="textarea"
     :class="classInstance"
     :value="value"
-    @input="onInput($event)"
     :placeholder="placeholder"
     :row="row"
-    ref="textarea"
     v-autosize
+    @input="onInput($event)"
   />
 </template>
+
+<script>
+export default {
+  name: 'Textarea',
+  props: {
+    type: String,
+    value: String,
+    emitInputEvents: Boolean,
+    placeholder: String,
+    row: Number,
+  },
+  computed: {
+    classInstance() {
+      return {
+        'subcomment-textarea-edit': this.type === 'subcommentEdit',
+        'subreply-textarea': this.type === 'subcommentReply',
+        'main-reply-textarea': !this.type,
+      };
+    },
+  },
+  mounted() {
+    this.$refs.textarea.focus();
+  },
+  methods: {
+    onInput($event) {
+      if (this.emitInputEvents) {
+        this.$emit('input', $event);
+      }
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 $borderColor: #ddd;
 $placeholderColor: #a2a2a2;
 
 textarea {
-  margin:0;
-  padding:0;
-  display:inline-block;
-  vertical-align:middle;
-  background:white;
+  margin: 0;
+  padding: 0;
+  display: inline-block;
+  vertical-align: middle;
+  background: white;
   width: 100%;
   border: 1px solid $borderColor;
   resize: none;
@@ -50,39 +82,7 @@ textarea::-webkit-input-placeholder {
   display: block;
   font-size: 14px;
   min-height: 70px;
-  overflow-y:hidden !important;
+  overflow-y: hidden !important;
   padding: 5px;
 }
 </style>
-
-<script>
-export default {
-  name: 'B3Textarea',
-  computed: {
-    classInstance() {
-      return {
-        'subcomment-textarea-edit': this.type === 'subcommentEdit',
-        'subreply-textarea': this.type === 'subcommentReply',
-        'main-reply-textarea': !this.type,
-      };
-    },
-  },
-  mounted() {
-    this.$refs.textarea.focus();
-  },
-  props: {
-    type: String,
-    value: String,
-    emitInputEvents: Boolean,
-    placeholder: String,
-    row: Number,
-  },
-  methods: {
-    onInput($event) {
-      if (this.emitInputEvents) {
-        this.$emit('input', $event);
-      }
-    },
-  },
-};
-</script>
