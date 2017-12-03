2e78f3dbf7cf3c10291fe73a987660891cd7b4b1
Kevin Peters <kpe@3yourmind.com>
2017-12-03 11:50:16 +0100

    test(button): add more tests for rendering button right

--- test/Button.spec.js
+++ test/Button.spec.js
@@ -4,7 +4,6 @@ import Button from '@/components/Button.vue';
 
 describe('Button.vue', () => {
   it('check if just one div is visible', () => {
-    const items = ['', ''];
     const wrapper = shallow(Button);
     wrapper.setProps({
       text: 'bar',
@@ -15,5 +14,27 @@ describe('Button.vue', () => {
       onClick: () => {},
     });
     expect(wrapper.findAll('div')).toHaveLength(1);
+  });
+
+  it('check if text is rendered right', () => {
+    const wrapper = shallow(Button);
+    wrapper.setProps({
+      text: 'foo',
+      warning: false,
+      disabled: false,
+      black: false,
+      gray: false,
+      onClick: () => {},
+    });
+    expect(wrapper.find('div').text()).toBe('foo');
+    wrapper.setProps({
+      text: 'bar',
+      warning: false,
+      disabled: false,
+      black: false,
+      gray: false,
+      onClick: () => {},
+    });
+    expect(wrapper.find('div').text()).toBe('bar');
   });
 });
