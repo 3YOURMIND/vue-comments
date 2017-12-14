a35be6734728ece920c138ae1f0cfd19d1c56a93
Kevin Peters <kpe@3yourmind.com>
2017-12-14 10:57:59 +0100

    feat(storybook): add stories for textarea

--- stories/index.js
+++ stories/index.js
@@ -1,20 +1,56 @@
 import {
   storiesOf
 } from 'storybook-vue';
-
-import VcoTitle from '../src/components/Title.vue';
 import stringifyObject from 'stringify-object';
+import VcoTitle from '../src/components/Title.vue';
+import VcoTextarea from '../src/components/Textarea.vue';
 
-storiesOf('Title', module)
-  .add('Title with no line', () => {
-    const translationsObject = {
-      title: 'Whatever',
-    };
-    const translation = stringifyObject(translationsObject);
+storiesOf('Textarea', module)
+  .add('Default textarea', () => {
     return {
       components: {
-        VcoTitle
+        VcoTextarea,
       },
-      template: `<VcoTitle :showLine="true" :translations="${translation}" />`
+      template: `<VcoTextarea
+        :emitInputEvents="true"
+        :placeholder="'default placeholder'"
+      />`,
+    };
+  })
+  .add('With type subcommentEdit', () => {
+    return {
+      components: {
+        VcoTextarea,
+      },
+      template: `<VcoTextarea
+        :type="'subcommentEdit'"
+        :emitInputEvents="true"
+        :placeholder="'default placeholder'"
+      />`,
+    };
+  })
+  .add('With type subcommentReply', () => {
+    return {
+      components: {
+        VcoTextarea,
+      },
+      template: `<VcoTextarea
+        :type="'subcommentReply'"
+        :emitInputEvents="true"
+        :placeholder="'default placeholder'"
+      />`,
     };
   });
+
+storiesOf('Title', module).add('Title with no line', () => {
+  const translationsObject = {
+    title: 'Whatever',
+  };
+  const translation = stringifyObject(translationsObject);
+  return {
+    components: {
+      VcoTitle,
+    },
+    template: `<VcoTitle :showLine="true" :translations="${translation}" />`,
+  };
+});
