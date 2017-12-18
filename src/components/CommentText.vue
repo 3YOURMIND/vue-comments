efe28afd2169bea2c8c2564b0df2063655ad9bff
igeligel <kevinigeligeligel@gmail.com>
2017-12-18 21:45:02 +0100

    test(components): add test for comment text
    add test for comment text to differentiate right line splitting

--- src/components/CommentText.vue
+++ src/components/CommentText.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-xs-12">
+    <div class="col-xs-12" data-test="text-container">
       <span v-for="line in text.split('\n')" :key="line">
         {{line}}<br>
       </span>
---
+++ test/CommentText.spec.js
@@ -0,0 +1,29 @@
+import { shallow } from 'vue-test-utils';
+
+import CommentText from '../src/components/CommentText.vue';
+
+describe('comment text module', () => {
+  it('test single line text', () => {
+    const wrapper = shallow(CommentText, {
+      propsData: {
+        text: 'adwadwwad',
+      },
+    });
+    const spanContainer = wrapper.findAll(
+      '[data-test="text-container"] > span',
+    );
+    expect(spanContainer).toHaveLength(1);
+  });
+
+  it('test multi line text', () => {
+    const wrapper = shallow(CommentText, {
+      propsData: {
+        text: 'adwadwwad \n awddwawda \n \nd awdwda',
+      },
+    });
+    const spanContainer = wrapper.findAll(
+      '[data-test="text-container"] > span',
+    );
+    expect(spanContainer).toHaveLength(4);
+  });
+});
