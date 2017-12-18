0d74b3f2f421fc713615339696748093977840ce
igeligel <kevinigeligeligel@gmail.com>
2017-12-18 20:46:32 +0100

    test(components): test slot of modal
    test header slot of modal

--- test/Modal.spec.js
+++ test/Modal.spec.js
@@ -16,4 +16,15 @@ describe('Modal Module', () => {
     const modalHeader = wrapper.find('[data-test="vco-modal-header"]');
     expect(modalHeader.classes()).toContain('vco-modal__header');
   });
+
+  it('test slot header', () => {
+    const wrapper = shallow(Modal, {
+      slots: {
+        header: '<h2>New Header</h2>',
+      },
+    });
+    const modalHeaderText = wrapper.find('[data-test="vco-modal-header"] h2');
+    expect(modalHeaderText.exists()).toBeTruthy();
+    expect(modalHeaderText.text()).toBe('New Header');
+  });
 });
