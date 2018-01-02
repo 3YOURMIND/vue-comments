55886acaa881d9964dab0b7196764dabeaabb006
igeligel <kevinigeligeligel@gmail.com>
2018-01-02 21:01:33 +0100

    feat(stories): add stories
    add stories for modal

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,32 @@ import { storiesOf } from 'storybook-vue';
 import stringifyObject from 'stringify-object';
 import VcoTitle from '../src/components/Title.vue';
 import VcoTextarea from '../src/components/Textarea.vue';
+import Modal from '../src/components/Modal.vue';
+
+storiesOf('Modal', module)
+  .add('Slots provided', () => {
+    return {
+      components: {
+        Modal,
+      },
+      template: `<Modal>
+        <h4 slot="header">Delete Comments</h4>
+        <div slot="body">Are you sure to delete this comment?</div>
+        <div slot="footer">
+          <button class="vco-button--black" @click="hideModal">Close</button>
+          <button class="vco-button" @click="deleteComment">Delete Comment</button>
+        </div>
+      </Modal>`,
+    };
+  })
+  .add('No slots provided', () => {
+    return {
+      components: {
+        Modal,
+      },
+      template: `<Modal />`,
+    };
+  });
 
 storiesOf('Textarea', module)
   .add('Default textarea', () => {
