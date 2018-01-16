f3034769d758101c4e984f0825d21e24c852abfa
Kevin Peters <kpe@3yourmind.com>
2018-01-16 17:09:53 +0100

    chore(dependencies): upgrade dependencies
    upgrade dependencies and migrate to new vue test library

--- test/CommentNoContent.spec.js
+++ test/CommentNoContent.spec.js
@@ -1,4 +1,4 @@
-import { shallow } from 'vue-test-utils';
+import { shallow } from '@vue/test-utils';
 
 import CommentNoContent from '../src/components/CommentNoContent.vue';
 
--- test/Divider.spec.js
+++ test/Divider.spec.js
@@ -1,4 +1,4 @@
-import { shallow } from 'vue-test-utils';
+import { shallow } from '@vue/test-utils';
 
 import Divider from '../src/components/Divider.vue';
 
